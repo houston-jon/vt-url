84c6aef66a52b1539da3c1ea5193f566f219cd6d
houston-jon <44117980+houston-jon@users.noreply.github.com>
2018-10-14 10:29:01 -0500

    Update vt-url.py

--- vt-url.py
+++ vt-url.py
@@ -9,7 +9,3 @@ params = {'apikey': 'insert api key here', 'resource':'http://www.virustotal.com
 response = requests.post('https://www.virustotal.com/vtapi/v2/url/report', params=params, headers=headers)
 
 json_response = response.json()
-
-vt_data = json.loads(response.content)
-
-pprint.pprint(vt_data)
