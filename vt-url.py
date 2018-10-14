86e9c2d2b8096a66a70b804f9b4b2dbf230bb631
houston-jon <44117980+houston-jon@users.noreply.github.com>
2018-10-14 10:28:00 -0500

    Update vt-url.py

--- vt-url.py
+++ vt-url.py
@@ -4,7 +4,7 @@ headers = {
     "Accept-Encoding": "gzip, deflate",
     "User-Agent" : "gzip,  My Python requests library example client or username"
     }
-params = {'apikey': 'b82489dcd39ef37fc9dbe00c8c2b96309c203ac25e51f65b3190af749f466747', 'resource':'http://www.virustotal.com'}
+params = {'apikey': 'insert api key here', 'resource':'http://www.virustotal.com'}
 
 response = requests.post('https://www.virustotal.com/vtapi/v2/url/report', params=params, headers=headers)
 
